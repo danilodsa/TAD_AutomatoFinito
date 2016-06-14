4bf580c8021df1e5d44875794c445ed24462ad10
Danilo Alves <danilo.alves@gmail.com>
2016-06-14 01:34:36 -0300

    Commit Arquivos/Makefile

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: daniloalves
+ *
+ * Created on 14 de Junho de 2016, 01:33
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
