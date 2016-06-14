361146305d68eda55e9cec0065a0ab89b7ca7398
Danilo Alves <danilo.alves@gmail.com>
2016-06-14 01:55:33 -0300

    automato.c automato.h

---
+++ automato.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   automato.h
+ * Author: daniloalves
+ *
+ * Created on 14 de Junho de 2016, 01:49
+ */
+
+#ifndef AUTOMATO_H
+#define	AUTOMATO_H
+
+typedef struct Automato automato;
+
+void AFcriaEstado(automato* f, int e, int inicial, int final);
+
+
+#endif	/* AUTOMATO_H */
+
